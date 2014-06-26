d3ecefca096b6625dee5d7aacc0333c243df84d7
PlanetGeeks <PlanetGeekITA@gmail.com>
2014-06-26 16:43:59 +0200

    Episodio 1 :)

---
+++ Bukkit_Youtube/src/it/planetgeeks/bukkit/episodio1/Episodio1.java
@@ -0,0 +1,16 @@
+package it.planetgeeks.bukkit.episodio1;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Episodio1 extends JavaPlugin
+{
+    public void onEnable()
+    {
+    	System.out.println("PLUGIN CARICATO CORRETTAMENTE");
+    }
+    
+    public void onDisable()
+    {
+    	System.out.println("PLUGIN DISABILITATO CORRETTAMENTE");
+    }
+}
